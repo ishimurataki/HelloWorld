1ba8be472035fc3e06dcf626b2e3838e3757ac98
Matthew <matthew25kim@gmail.com>
2019-12-03 13:30:35 -0500

    adsorption algo

---
+++ adsorption/AdsorptionInitMapper.java
@@ -0,0 +1,26 @@
+import org.apache.hadoop.mapreduce.*;
+import java.io.IOException;
+
+import org.apache.hadoop.io.*;
+
+public class AdsorptionInitMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+	@Override
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		String line = value.toString();
+
+		if (!line.contains(":")) {
+			String name = line;
+			context.write(new Text(name), new Text('nothing'));
+		} else {
+			String[] splitNameInterests = line.split(":");
+			String[] splitInterests = splitNameInterests[1].split(",");
+			for (int i = 0; i < splitInterests.length; i++) {
+				String interest = splitInterests[i];
+				context.write(new Text(name), new Text(interest));
+			}
+		}
+
+
+	}
+}
